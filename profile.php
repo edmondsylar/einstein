39ab08da49bf1f029e6ff5a337782ca383f0dfd0
edmondsylar <edmondmusiitwa@gmail.com>
2019-12-14 16:05:54 +0300

    added to cloud

--- profile.php
+++ profile.php
@@ -152,15 +152,15 @@
 										<div class="portfolio-gallery-sec">
 											<h3>Gallery</h3>
 											<div class="gallery_pf">
-												<div class="row" style="z-index: 1;"">
+												<div class="row" style="z-index: 1;">
 
                           <?php foreach ($gallery as $gala): ?>
 								             <?php $img = base64_encode($gala['image']); ?>
     													<div class="col-lg-4 col-md-4 col-sm-4 col-6" class="galla-view" data-tooltip="<?php echo $gala['description']; ?>">
     														<div class="gallery_pt">
     															<img src="data:image/jpg;charset=utf8;base64,<?php echo $img ?>"  class=";" alt="gallery image">
-    															<!-- <a href="#"  class="overview-opening" title=""><img src="images/all-out.png" alt=""></a> -->
-    														</div><!--gallery_pt end-->
+    															<a href="#"  class="overview-opening" title=""><img src="images/all-out.png" alt=""></a>
+    														</div>
     													</div>
                           <?php endforeach; ?>
 
@@ -204,179 +204,6 @@
 		</main>
 
     <?php include_once 'includes/footer.php'; ?>
-
-		<div class="overview-box" id="overview-box">
-			<div class="overview-edit">
-				<h3>Add Gallery Image</h3>
-				<span>5000 character left</span>
-
-				<form action="backend/gallery.php" method="POST" enctype="multipart/form-data">
-          <input type="hidden" name="userid" value="<?php echo $_GET['view']; ?>">
-					<textarea name="description" placeholder="Description"></textarea>
-
-          <div class="row" style="margin: 10px;">
-            <div class="col-lg-12">
-              <div class="add-dp" id="OpenImgUpload">
-                <label for="file"><i class="fas fa-camera"></i></label>
-                <input type="file" id="file" name="image">
-                <label for="file">Upload Image</label>
-              </div>
-            </div>
-          </div>
-
-					<button type="submit" class="save">Save</button>
-					<button class="cancel">Cancel</button>
-				</form>
-				<a href="#" title="" class="close-box"><i class="la la-close"></i></a>
-			</div><!--overview-edit end-->
-		</div><!--overview-box end-->
-
-
-		<div class="overview-box" id="experience-box">
-			<div class="overview-edit">
-				<h3>Experience</h3>
-				<form>
-					<input type="text" name="subject" placeholder="Subject">
-					<textarea></textarea>
-					<button type="submit" class="save">Save</button>
-					<button type="submit" class="save-add">Save & Add More</button>
-					<button type="submit" class="cancel">Cancel</button>
-				</form>
-				<a href="#" title="" class="close-box"><i class="la la-close"></i></a>
-			</div><!--overview-edit end-->
-		</div><!--overview-box end-->
-
-		<div class="overview-box" id="education-box">
-			<div class="overview-edit">
-				<h3>Education</h3>
-				<form>
-					<input type="text" name="school" placeholder="School / University">
-					<div class="datepicky">
-						<div class="row">
-							<div class="col-lg-6 no-left-pd">
-								<div class="datefm">
-									<input type="text" name="from" placeholder="From" class="datepicker">
-									<i class="fa fa-calendar"></i>
-								</div>
-							</div>
-							<div class="col-lg-6 no-righ-pd">
-								<div class="datefm">
-									<input type="text" name="to" placeholder="To" class="datepicker">
-									<i class="fa fa-calendar"></i>
-								</div>
-							</div>
-						</div>
-					</div>
-					<input type="text" name="degree" placeholder="Degree">
-					<textarea placeholder="Description"></textarea>
-					<button type="submit" class="save">Save</button>
-					<button type="submit" class="save-add">Save & Add More</button>
-					<button type="submit" class="cancel">Cancel</button>
-				</form>
-				<a href="#" title="" class="close-box"><i class="la la-close"></i></a>
-			</div><!--overview-edit end-->
-		</div><!--overview-box end-->
-
-		<div class="overview-box" id="location-box">
-			<div class="overview-edit">
-				<h3>Location</h3>
-				<form>
-					<div class="datefm">
-						<select>
-							<option>Country</option>
-							<option value="pakistan">Pakistan</option>
-							<option value="england">England</option>
-							<option value="india">India</option>
-							<option value="usa">United Sates</option>
-						</select>
-						<i class="fa fa-globe"></i>
-					</div>
-					<div class="datefm">
-						<select>
-							<option>City</option>
-							<option value="london">London</option>
-							<option value="new-york">New York</option>
-							<option value="sydney">Sydney</option>
-							<option value="chicago">Chicago</option>
-						</select>
-						<i class="fa fa-map-marker"></i>
-					</div>
-					<button type="submit" class="save">Save</button>
-					<button type="submit" class="cancel">Cancel</button>
-				</form>
-				<a href="#" title="" class="close-box"><i class="la la-close"></i></a>
-			</div><!--overview-edit end-->
-		</div><!--overview-box end-->
-
-		<div class="overview-box" id="skills-box">
-			<div class="overview-edit">
-				<h3>Skills</h3>
-				<ul>
-					<li><a href="#" title="" class="skl-name">HTML</a><a href="#" title="" class="close-skl"><i class="la la-close"></i></a></li>
-					<li><a href="#" title="" class="skl-name">php</a><a href="#" title="" class="close-skl"><i class="la la-close"></i></a></li>
-					<li><a href="#" title="" class="skl-name">css</a><a href="#" title="" class="close-skl"><i class="la la-close"></i></a></li>
-				</ul>
-				<form>
-					<input type="text" name="skills" placeholder="Skills">
-					<button type="submit" class="save">Save</button>
-					<button type="submit" class="save-add">Save & Add More</button>
-					<button type="submit" class="cancel">Cancel</button>
-				</form>
-				<a href="#" title="" class="close-box"><i class="la la-close"></i></a>
-			</div><!--overview-edit end-->
-		</div><!--overview-box end-->
-
-		<div class="overview-box" id="create-portfolio">
-			<div class="overview-edit">
-				<h3>Create Portfolio</h3>
-				<form>
-					<input type="text" name="pf-name" placeholder="Portfolio Name">
-					<div class="file-submit nomg">
-						<input type="file" name="file">
-					</div>
-					<div class="pf-img">
-						<img src="images/resources/np.png" alt="">
-					</div>
-					<input type="text" name="website-url" placeholder="htp://www.example.com">
-					<button type="submit" class="save">Save</button>
-					<button type="submit" class="cancel">Cancel</button>
-				</form>
-				<a href="#" title="" class="close-box"><i class="la la-close"></i></a>
-			</div><!--overview-edit end-->
-		</div><!--overview-box end-->
-
-		<div class="overview-box" id="establish-box">
-			<div class="overview-edit">
-				<h3>Establish Since</h3>
-				<form>
-					<div class="daty">
-						<input type="text" name="establish" placeholder="Select Date" class="datepicker">
-						<i class="fa fa-calendar"></i>
-					</div>
-					<button type="submit" class="save">Save</button>
-					<button type="submit" class="cancel">Cancel</button>
-				</form>
-				<a href="#" title="" class="close-box"><i class="la la-close"></i></a>
-			</div><!--overview-edit end-->
-		</div><!--overview-box end-->
-
-
-		<div class="overview-box" id="total-employes">
-			<div class="overview-edit">
-				<h3>Total Employees</h3>
-				<form>
-					<input type="text" name="employes" placeholder="Type in numbers">
-					<button type="submit" class="save">Save</button>
-					<button type="submit" class="cancel">Cancel</button>
-				</form>
-				<a href="#" title="" class="close-box"><i class="la la-close"></i></a>
-			</div><!--overview-edit end-->
-		</div><!--overview-box end-->
-
-
-
-	</div><!--theme-layout end-->
-
   <script>
     (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
