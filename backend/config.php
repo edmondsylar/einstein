46ad102c77c28d376e591c1c97c261656492f9ac
edmondsylar <edmondmusiitwa@gmail.com>
2019-12-14 15:46:14 +0300

    pushed:

--- backend/config.php
+++ backend/config.php
@@ -15,7 +15,7 @@
     {
       define('DB_SERVER', 'localhost');
       define('DB_USERNAME', 'root');
-      define('DB_PASSWORD', '');
+      define('DB_PASSWORD', 'root');
       define('DB_NAME', 'raising');
 
       $this->connector = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
